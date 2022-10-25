cff1e02a7df8ed1a9f12fd834eed82d3ce8c21a7
Tomas <tomasao87@gmail.com>
2022-10-25 17:10:30 -0300

    primer push

---
+++ script.js
@@ -0,0 +1,7 @@
+alert("Bienvenido");
+let nombreUsuario = String(prompt("Ingrese su nombre "));
+if (nombreUsuario !=" "){
+    console.log("Bienvenido" + " " + nombreUsuario );
+}else{
+    console.log("Error: Ingrese un Nombre");
+}
